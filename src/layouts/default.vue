0e886a3d9deac502c9497daefd9f9fe3d7a0062b
Paloma Tejeda <palomatejeda81@gmail.com>
2023-02-03 11:48:22 +0100

    layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <nav>
+      <NuxtLink to="/">HOME (index)</NuxtLink> |
+      <NuxtLink to="/other">OTHER PAGE</NuxtLink>
+    </nav>
+    <main><slot /></main>
+  </div>
+</template>
+
+<script setup></script>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Index page</h1>
+</template>
+
+<script>
+export default {
+  layout: "default",
+};
+</script>
+
+<style></style>
