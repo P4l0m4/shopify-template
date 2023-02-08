ccb8b2a330b5012558b87159f2e046c6a9918fc2
Paloma Tejeda <palomatejeda81@gmail.com>
2023-02-08 17:01:58 +0100

    m

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   srcDir: "src/",
-
   modules: [
     // ...
     "@pinia/nuxt",
   ],
+  // shopify: {
+  //   domain: "https://a3abd3.myshopify.com/",
+
+  //   storefrontAccessToken: "6dc6447af34112415032da070cd6f29c",
+  // },
 });
