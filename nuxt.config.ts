aaff289db7ecc931af96cbb3336cf541237b8eef
Paloma Tejeda <palomatejeda81@gmail.com>
2023-04-07 15:18:14 +0200

    yotpot installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,4 +18,14 @@ export default defineNuxtConfig({
   sitemap: {
     siteUrl: 'nuxt3-shopify-template.netlify.app',
   },
+  app: {
+    head: {
+      script: [
+        {
+          children:
+            '(function e(){var e=document.createElement("script");e.type="text/javascript",e.async=true,e.src="//staticw2.yotpo.com/g0YglhE3q3wxCEFpK4R4tF5UE349zZrrw0C4psLQ/widget.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();',
+        },
+      ],
+    },
+  },
 })
