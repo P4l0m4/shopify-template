77c5e6f0e79cc4a4d22b51edd1838dc64b321628
Paloma Tejeda <palomatejeda81@gmail.com>
2023-05-05 15:09:55 +0200

    mc

--- src/pages/products/[slug].vue
+++ src/pages/products/[slug].vue
@@ -157,7 +157,7 @@ useHead({
       ></div>
       <!-- <div id="yotpo-testimonials-custom-tab"></div> -->
     </section>
-    <p class="title">Nos best sellers</p>
+    <p class="container__title">Nos best sellers</p>
     <ProductsPropositions />
   </div>
 </template>
@@ -165,8 +165,14 @@ useHead({
 .container {
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 4rem;
   padding: 1rem 0;
+
+  &__title {
+    font-weight: 200 !important;
+    font-size: 1.5rem;
+    padding: 0 1rem;
+  }
 }
 .product {
   display: flex;
