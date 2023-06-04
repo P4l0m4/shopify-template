dcddfdd0b731ad8e5dd88beadf4f845b8603c092
Paloma Tejeda <palomatejeda81@gmail.com>
2023-06-04 14:48:23 +0200

    mc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,19 +37,18 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      //       script: [
-      //         {
-      //           hid: 'gtmHead',
-      //           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      //       })(window, document, 'script', 'dataLayer', 'GTM-MQVVM37');`,
-      //           body: true,
-      //           defer: true,
-      //           async: false,
-      //         },
-      //       ],
+      script: [
+        {
+          hid: 'gtmHead',
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window, document, 'script', 'dataLayer', 'GTM-MQVVM37');`,
+          body: true,
+          defer: true,
+        },
+      ],
       htmlAttrs: { lang: 'fr' },
     },
   },
